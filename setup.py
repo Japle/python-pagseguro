0d1081591196fab555481d1a78cc06e6e8d77660
Bruno Rocha <rochacbruno@gmail.com>
2013-10-30 00:54:04 -0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.md').read()
+requirements = ['requests', 'xmltodict', 'arrow']
+
+setup(
+    name='pagseguro',
+    version='0.1.0.dev0',
+    description='Pagseguro API v2 wrapper',
+    author='Bruno Rocha',
+    author_email='rochacbruno@gmail.com',
+    url='https://github.com/rochacbruno/python-pagseguro',
+    packages=[
+        'pagseguro',
+    ],
+    package_dir={'pagseguro': 'pagseguro'},
+    include_package_data=True,
+    install_requires=requirements,
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    keywords='pagseguro, payment, payments, credit-card'
+)
