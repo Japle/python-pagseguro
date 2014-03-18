259ed3aa743778be19006d1a9240debb159ce96d
shyba <victor.shyba@gmail.com>
2014-03-18 03:44:54 -0300

    adding tests to list, add and remove products

--- examples/flask/tests.py
+++ examples/flask/tests.py
@@ -4,6 +4,7 @@ import flask_seguro
 import unittest
 import tempfile
 import flask
+from flask import json
 
 class FlasKSeguroTestCase(unittest.TestCase):
 
@@ -17,11 +18,25 @@ class FlasKSeguroTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flask_seguro.app.config['DATABASE'])
 
-    def testRetrieveCart(self):
+
+    def testRetrieveCartAndAddRemoveItem(self):
         with flask_seguro.app.test_client() as c:
-            #assert 'cart' not in flask.session
             response = c.get('/')
-            assert 'cart' in flask.session
+            session = flask.session
+            assert 'cart' in session
+            assert len(session['cart'])==0
+
+            response = c.get('/products/list')
+            products = json.loads(response.data)
+            assert 'product_list' in products
+            assert len(products['product_list']) > 0
+            products = products['product_list']
+
+            response = c.get('/cart/add/%s'%(products[0]['id']))
+            assert len(session['cart'])==1
+
+            response = c.get('/cart/remove/%s'%(products[0]['id']))
+            assert len(session['cart'])==0
 
 
 if __name__ == '__main__':
