63c44fcac3d0c9afe7d4cd96db972c04082fb652
shyba <victor.shyba@gmail.com>
2014-03-19 13:39:42 -0300

    refactoring

--- examples/flask/flask_seguro/cart.py
+++ examples/flask/flask_seguro/cart.py
@@ -1,30 +1,42 @@
-from pagseguro import PagSeguro
+from flask_seguro.products import Products
+from flask import current_app as app
 
 
-def make_cart():
-    # aqui o carrinho poderia ser criado no banco, por exemplo
-    return []
+class Cart:
 
-def make_pg():
-    pg = PagSeguro(email="seuemail@dominio.com", token="ABCDEFGHIJKLMNO")
-    pg.sender = {
-        "name": "Bruno Rocha",
-        "area_code": 11,
-        "phone": 981001213,
-        "email": "rochacbruno@gmail.com",
-    }
-    pg.shipping = {
-        "type": pg.SEDEX,
-        "street": "Av Brig Faria Lima",
-        "number": 1234,
-        "complement": "5 andar",
-        "district": "Jardim Paulistano",
-        "postal_code": "06650030",
-        "city": "Sao Paulo",
-        "state": "SP",
-        "country": "BRA"
-    }
-    pg.extra_amount = 12.70
-    pg.redirect_url = "http://meusite.com/obrigado"
-    pg.notification_url = "http://meusite.com/notification"
-    return pg
+    def __init__(self, cart_dict={}):
+        if cart_dict == {}:
+            self.total = 0
+            self.subtotal = 0
+            self.items = []
+        else:
+            self.total = cart_dict["total"]
+            self.subtotal = cart_dict["subtotal"]
+            self.items = cart_dict["items"]
+
+    def to_dict(self):
+        return {"total": self.total,
+                "subtotal": self.subtotal,
+                "items": self.items}
+
+    def change_item(self, item_id, operation):
+        product = Products().get_one(item_id)
+        if product:
+            if operation == 'add':
+                self.items.append(product)
+            elif operation == 'remove':
+                self.items.remove(product)
+            self.update()
+            return True
+        else:
+            return False
+
+    def update(self):
+        subtotal = float(0)
+        total = float(0)
+        for product in self.items:
+            subtotal += float(product["price"])
+        if subtotal > 0:
+            total = subtotal + float(app.config['EXTRA_AMOUNT'])
+        self.subtotal = subtotal
+        self.total = total
---
+++ examples/flask/flask_seguro/products.py
@@ -0,0 +1,19 @@
+class Products:
+
+    def __init__(self):
+        self.products = [
+            {"id": "0001", "description": "Produto 1", "amount":
+                354.20, "quantity": 2, "weight": 200, "price": 10.10},
+            {"id": "0002", "description": "Produto 2", "amount":
+                50, "quantity": 1, "weight": 1000, "price": 10.50},
+        ]
+
+    def get_all(self):
+        return self.products
+
+    def get_one(self, item_id):
+        p = filter(lambda p: p['id'] == item_id, self.products)
+        if len(p) > 0:
+            return p[0]
+        else:
+            return False
