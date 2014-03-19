ea8e061eb08904984e7a348e7cfc04f050e16304
shyba <victor.shyba@gmail.com>
2014-03-19 13:39:22 -0300

    adding config class

--- examples/flask/run.py
+++ examples/flask/run.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 from flask_seguro import app
 
+
+class Config(object):
+    EXTRA_AMOUNT = 12.70
+    REDIRECT_URL = "http://meusite.com/obrigado"
+    NOTIFICATION_URL = "http://meusite.com/notification"
+    EMAIL = "seuemail@dominio.com"
+    TOKEN = "ABCDEFGHIJKLMNO"
+
 if __name__ == "__main__":
+    app.config.from_object(Config())
     app.run()
