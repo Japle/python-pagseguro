d8ca54c57718e58f85ec536c3ec9762753663bcb
shyba <victor.shyba@gmail.com>
2014-03-21 21:24:24 -0300

    quantity=1 since we dont use it yet

--- examples/flask/flask_seguro/products.py
+++ examples/flask/flask_seguro/products.py
@@ -3,7 +3,7 @@ class Products:
     def __init__(self):
         self.products = [
             {"id": "0001", "description": "Produto 1", "amount":
-                354.20, "quantity": 2, "weight": 200, "price": 10.10},
+                1.00, "quantity": 1, "weight": 200, "price": 10.10},
             {"id": "0002", "description": "Produto 2", "amount":
                 50, "quantity": 1, "weight": 1000, "price": 10.50},
         ]
