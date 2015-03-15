3d33eadc19d99c5e2ed0838824ac7b2084310752
David Kwast <david@kwast.net>
2015-03-14 23:27:23 -0300

    Sandbox support

--- pagseguro/configs.py
+++ pagseguro/configs.py
@@ -2,25 +2,34 @@
 
 
 class Config(object):
+    
+    def __init__(self, sandbox=False):
+        
+        if sandbox:
+            self.BASE_URL = 'https://ws.sandbox.pagseguro.uol.com.br'
+            self.PAYMENT_HOST = "https://sandbox.pagseguro.uol.com.br"
+        else:
+            self.BASE_URL = "https://ws.pagseguro.uol.com.br"
+            self.PAYMENT_HOST = "https://pagseguro.uol.com.br"
+        
+        self.VERSION = "/v2/"
+        self.CHECKOUT_SUFFIX = self.VERSION + "checkout"
+        self.CHARSET = "UTF-8"  # ISO-8859-1
+        self.NOTIFICATION_SUFFIX = self.VERSION + "transactions/notifications/%s"
+        self.TRANSACTION_SUFFIX = self.VERSION + "transactions/%s"
+        self.QUERY_TRANSACTION_SUFFIX = self.VERSION + "transactions"
+        self.CHECKOUT_URL = self.BASE_URL + self.CHECKOUT_SUFFIX
+        self.NOTIFICATION_URL = self.BASE_URL + self.NOTIFICATION_SUFFIX
+        self.TRANSACTION_URL = self.BASE_URL + self.TRANSACTION_SUFFIX
+        self.QUERY_TRANSACTION_URL = self.BASE_URL + self.QUERY_TRANSACTION_SUFFIX
+        self.CURRENCY = "BRL"
+        self.CTYPE = "application/x-www-form-urlencoded; charset={0}".format(self.CHARSET)
+        self.HEADERS = {"Content-Type": self.CTYPE}
+        self.REFERENCE_PREFIX = "REF%s"
+        self.PAYMENT_URL = self.PAYMENT_HOST + self.CHECKOUT_SUFFIX + "/payment.html?code=%s"
+        
+        self.DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
+    
     def get(self, key, default=None):
         return getattr(self, key, default)
 
-    BASE_URL = "https://ws.pagseguro.uol.com.br"
-    VERSION = "/v2/"
-    CHECKOUT_SUFFIX = VERSION + "checkout"
-    CHARSET = "UTF-8"  # ISO-8859-1
-    NOTIFICATION_SUFFIX = VERSION + "transactions/notifications/%s"
-    TRANSACTION_SUFFIX = VERSION + "transactions/%s"
-    QUERY_TRANSACTION_SUFFIX = VERSION + "transactions"
-    CHECKOUT_URL = BASE_URL + CHECKOUT_SUFFIX
-    NOTIFICATION_URL = BASE_URL + NOTIFICATION_SUFFIX
-    TRANSACTION_URL = BASE_URL + TRANSACTION_SUFFIX
-    QUERY_TRANSACTION_URL = BASE_URL + QUERY_TRANSACTION_SUFFIX
-    CURRENCY = "BRL"
-    CTYPE = "application/x-www-form-urlencoded; charset={0}".format(CHARSET)
-    HEADERS = {"Content-Type": CTYPE}
-    REFERENCE_PREFIX = "REF%s"
-    PAYMENT_HOST = "https://pagseguro.uol.com.br"
-    PAYMENT_URL = PAYMENT_HOST + CHECKOUT_SUFFIX + "/payment.html?code=%s"
-
-    DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S'
