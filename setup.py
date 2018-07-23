be6708d385292c7adda3122ba6dd74f11dc1419f
Patrick Mazulo <pmazulo@gmail.com>
2018-07-23 20:41:46 -0300

    Bump package version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('requirements.txt') as reqs:
 
 setup(
     name='pagseguro',
-    version='0.3.2',
+    version='0.3.3',
     description='Pagseguro API v2 wrapper',
     author='Bruno Rocha',
     author_email='rochacbruno@gmail.com',
